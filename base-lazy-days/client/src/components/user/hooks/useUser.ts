0decf6fe47be3a54efd4817f9ea9288954cf6ceb
Ian Gardocki <ian.gardocki@wavelengthusaf.com>
2024-11-12 19:53:25 -0600

    finish auth section

--- base-lazy-days/client/src/components/user/hooks/useUser.ts
+++ base-lazy-days/client/src/components/user/hooks/useUser.ts
@@ -5,31 +5,51 @@ import type { User } from "@shared/types";
 import { useLoginData } from "@/auth/AuthContext";
 import { axiosInstance, getJWTHeader } from "@/axiosInstance";
 import { queryKeys } from "@/react-query/constants";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
+import { generateUserKey } from "@/react-query/key-factories";
 
 // query function
-// async function getUser(userId: number, userToken: string) {
-//   const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
-//     `/user/${userId}`,
-//     {
-//       headers: getJWTHeader(userToken),
-//     }
-//   );
+async function getUser(userId: number, userToken: string) {
+  const { data }: AxiosResponse<{ user: User }> = await axiosInstance.get(
+    `/user/${userId}`,
+    {
+      headers: getJWTHeader(userToken),
+    }
+  );
 
-//   return data.user;
-// }
+  return data.user;
+}
 
 export function useUser() {
-  // TODO: call useQuery to update user data from server
-  const user: User = null;
+  const queryClient = useQueryClient();
+
+  // get details on userid
+  const {userId, userToken} = useLoginData();
+
+  // call useQuery to update user data from server
+  const {data:user} = useQuery({
+    // if not enabled, query will not run
+    enabled: !!userId, // if user id is truthy, this is true & if its falsey, this is false
+    queryKey: generateUserKey(userId, userToken),
+    queryFn: ()=> getUser(userId, userToken),
+    staleTime: Infinity // data is never stale unless user updates it
+  })
 
   // meant to be called from useAuth
   function updateUser(newUser: User): void {
-    // TODO: update the user in the query cache
+    // update the user in the query cache
+    queryClient.setQueryData(
+      generateUserKey(newUser.id, newUser.token),
+      newUser
+    )
   }
 
   // meant to be called from useAuth
   function clearUser() {
-    // TODO: reset user to null in query cache
+    // reset user to null in query cache
+    queryClient.removeQueries({
+      queryKey: [queryKeys.appointments, queryKeys.user]
+    })
   }
 
   return { user, updateUser, clearUser };
